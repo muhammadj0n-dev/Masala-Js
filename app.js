6fbcde5dd1cd3befa086f6b23488af592ba384aa
muhammadj0n-dev <153054724+muhammadj0n-dev@users.noreply.github.com>
2024-01-08 15:31:17 +0500

    JSda masala yechish

---
+++ app.js
@@ -0,0 +1,178 @@
+// <---- 4-misol
+// let number = +prompt("Please enter a number")
+// if(Number.isInteger(number && number % 2 == 0)){
+// console.log("Siz kiritgan son butun va juft son ")
+// }else{
+//     console.log("Agar shart false kasr va toq son")
+// }
+
+// 6 - masala
+// let a =1;
+// let b = 2;
+// let c = 5;
+// if (b > a && b < c){
+//     console.log(true)
+// }else {
+//     console.log(false)
+// }
+// 7 - ma sala
+// let a = 5;
+// let b = 8;
+// let c = 10;
+// if(a < b && b < c && a < c ){
+//     console.log(true)
+// }else {
+//     console.log(false)
+// }
+// 8-masala 
+// let a = 5;
+// let b = 6;
+// let c = 7;
+// let d = 8;
+// if( a > 0 && b > 0 && c > 0 && d > 0) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+// 9-masala
+// let a = +prompt("a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+// let d = +prompt("d sonni kiriting")
+// if (( a > 0 && b > 0  && d > 0)|| (a > 0 && b > 0 && d < 0 ) || (a < 0 && b > 0 && d > 0 ) || (a > 0 && b > 0 && d > 0 ) ){
+//     console.log(true)
+// } else {
+//     (console.log(false))
+// }
+// 10-masala
+// let a = +prompt("Hafta kunlarini sonlar kesimida kiritin (1-7)")
+// if (a==1){
+//     console.log('${a} - kun duyshanba')
+// } else if (a==2){
+//     console.log('${a} - kun seyshanba')
+// } else if (a==3){
+//     console.log('${a} - kun chorshanba')
+// }else if (a==4){
+//     console.log('${a} - kun payshanba')
+// }else if (a==5){
+//     console.log('${a} - kun juma')
+// }else if (a==6){
+//     console.log('${a} - kun shanba')
+// }else if(a==7){
+//     console.log('${a} - kun yakshaba')
+// }
+// 11- masala
+
+// let phone = prompt("Telefon raqamingizni kiriting:");
+
+// let phoneCode = phone[0] + phone[1];
+
+// if(phoneCode === "99"){
+//     document.body.innerHTML = Uzmobile nomer: $ {phone};
+// }else if(phoneCode === "97"){
+//     document.body.innerHTML = Mobiuz nomer: ${ phone};
+// }else if(phoneCode === "94"){
+//     document.body.innerHTML = Usell nomer: ${ phone};
+// }else if(phoneCode === "90"){
+//     document.body.innerHTML = Beeline nomer: ${ phone};
+// }
+
+// 12-masala
+// let number = +prompt("Istalgan turdagi sonni kiriting;");
+// if ( number > 0){
+//         console.log(number + 1)
+// }else if (number < 0){
+//     console.log(number + -1)
+// }
+// 13- masala
+// let number = +prompt("Istalgan turdagi sonni kiriting");
+// if (number > 0){
+//     console.log(number + 3)
+// } else if (number < 0){
+//  console.log( number + 2)
+// }
+// 14 - masala
+// let a = +prompt("Istalgan turdagi sonni kiriting")
+// let b = +prompt("Istalgan turdagi sonni kiritng")
+// console.log(Math.max(a , b))
+// 15-masala
+// let a = +prompt("Istalgan turdagi sonni kiriting");
+// let b = +prompt("Istalgan turdagi sonni kiritng");
+// let d = +prompt("Istalgan turdagi sonni kiritng");
+// console.log(Math.max(a , b , d))
+// 16-masala
+// let a = +prompt("Istalgan turdagi sonni kiriting");
+// let b = +prompt("Istalgan turdagi sonni kiritng");
+// console.log(Math.min(a , b ))
+// 18-masala
+// let a = +prompt("Imtihonlardan to'plagan balingizni kiriting")
+// if (a > 50){
+//     document.body.innerHTML="Qoyil " + a +"Prizident Stipnediyasi.(1.000.000";
+// } else if( a < 50){
+//     document.body.innerHTML="Yomon emas" + a + "500.000 so'm";
+// } else if( a < 35){
+//     document.body.innerHTML="O'z ustingizda ishlang !" + a + "Stipendiyasiz";
+// } else if( a < 25){
+//     document.body.innerHTML="Imtihondan yiqildizngiz !" + a + "Goodbye";
+// }
+// 19 - masala
+// let n = prompt("Sonni kitiing:");
+// if(n > 0 && n % 2 == 0){
+//     document.body.innerHTML = Musbat juft;
+// }else if(n > 0 && n % 2 == 1){
+//     document.body.innerHTML = Musbat toq;
+// }else if(n < 0 && n % 2 == 0){
+//     document.body.innerHTML = Manfiy juft;
+// }else{
+//     document.body.innerHTML = Manfiy toq;
+// }
+// 20-masala
+// let a = +prompt("Tugilgan Yilingizni kiriting")
+// document.body.innerHTML=2024-a;
+// 21-masala
+// let a = +prompt("Son kiritng");
+// let b = +prompt("Son kiritng");
+// let c = +prompt("Son kiritng");
+// if(a <=b<=c){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+// 22-masala
+// let a = +prompt("Son kiriting");
+// let b = +prompt("Son kiritng");
+// if (a % 2 ==1 && b % 2 ==0){
+//     console.log(true)
+// }else if(a % 2 ==0 && b % 2 ==1 ){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+// 23-masala
+// let a = +prompt("Sonni kiriting");
+// let b = +prompt("Sonni kiriting");
+// let c = +prompt("Sonni kiriting");
+// if (a> 0 && b > 0 && c > 0){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+// 24-masala
+// let a = +prompt("Sonni kiriting");
+// let b = +prompt("Sonni kiriting");
+// let c = +prompt("Sonni kiriting");
+// if (a > 0 && b < 0 && c > 0){
+//     console.log(true)
+// }else if (a < 0 && b > 0 && c > 0){
+//     console.log(true)
+// }else if (a > 0 && b > 0 && c < 0){
+//     console.log(true)
+// }else if (a > 0 && b > 0 && c > 0) {
+//     console.log(false)
+// }
+
+
+
+
+
+
+
